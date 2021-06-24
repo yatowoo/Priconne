9ba8a16319ac1c4876a17ca383ccff9bff1fb0de
Yatowoo <yitao.wu@cern.ch>
2021-06-24 20:04:11 +0200

    APP - Test python package with Flask

--- RunTermux.py
+++ RunTermux.py
@@ -1,12 +1,23 @@
 import subprocess
+import priconne
 from flask import Flask,request, render_template
 
-app = Flask(__name__)
+app = Flask('priconne')
 
 @app.route('/',methods=["GET","POST"])
 def playsound():
   if request.method == 'GET':
-    return 'Welcome to Priconne'
+    return render_template('index.html')
+
+@app.route('/index.html',methods=["GET","POST"])
+def query():
+  if request.method == 'GET':
+    return render_template('index.html')
+  if request.method == 'POST':
+    tpBoost = int(request.form['tp_boost'])
+    hpMax = int(request.form['hp_max'])
+    data = priconne.boss_early_ub(tpBoost, hpMax)
+    return render_template('index.html', data=repr(data))
 
 if __name__ =='__main__':
   app.run(debug=True)
